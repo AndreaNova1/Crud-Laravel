894746c89c9bd4d82130fa2c21eed52b21f1c43e
AndreaNova1 <anoval@miumg.edu.gt>
2021-10-30 04:26:17 -0600

    implementacion de opcion de regresar a la vista

--- resources/views/usuarios/editform.blade.php
+++ resources/views/usuarios/editform.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'User Update')
 
 @section('content')
-    <div class="container mt-5">
+    <div class="container ">
         <div class="row justify-content-center">
             <div class="col-md-7 mt-5">
                 <!--Mensaje flash-->
@@ -23,27 +23,37 @@
                 @endif
 
                 <div class="card">
-                    <form action="{{ route('edit', $usuario->id)}}" method="POST" enctype="multipart/form-data">//enviamos la informacion
+                    <form action="{{ route('edit', $usuario->id)}}" method="POST" enctype="multipart/form-data">
                         @csrf @method('PATCH')
-                        <div class="card-header  text-center  p-3 mb-2 bg-info text-white">MODIFICAR USUARIO</div>
-                        <td>
-                            <img src="{{ asset('storage').'/'.$usuario->foto}}" alt="" height="90">
-                        </td>
+                        <div class="card-header  text-center  p-4 mb-2 bg-info text-white">MODIFICAR USUARIO</div>
+
                         <div class="card-body">
-                            <div class="row form-group">
-                                <label for="" class="col-4">Ingresa tu Nombre</label>
-                                <input type="text" name="nombre" class="form-control col-md-7" value="{{ $usuario->nombre }}">
+
+
+                            <div class="mb-3">
+                                <label class="form-label">Ingresa tu Nombre</label>
+                                <input type="text" name="nombre" class="form-control border border-info " value="{{ $usuario->nombre }}">
                             </div>
 
-                            <div class="row form-group">
-                                <label for="" class="col-2">Email</label>
-                                <input type="text" name="email" class="form-control col-md-9" value="{{ $usuario->email }}">
+                            <div class="mb-3">
+                                <label class="form-label">Email</label>
+                                <input type="text" name="email" class="form-control border border-info" value="{{ $usuario->email }}">
                             </div>
 
-                            <div class="row form-group">
-                                <label for="" class="col-2">Foto</label>
-                                <input type="file" name="foto" class="form-control col-md-9" value="{{ $usuario->foto }}">
+                            <div class="mb-3">
+                                <label class="form-label">Foto</label>
+                                <input type="file" name="foto" class="form-control border border-info" value="{{ $usuario->foto }}">
                             </div>
+
+
+
+
+                            <td>
+                                <div  align="center">
+                                    <img class="mb-2" src="{{ asset('storage').'/'.$usuario->foto}}" alt="" height="90">
+                                    <br>
+                                </div>
+                            </td>
 
                             <div class="row form-group">
                                 <label for="" class="col-2">Rol</label>
@@ -57,7 +67,8 @@
                             </div>
 
                             <div class="row form-group">
-                                <button type="submit" class="btn btn-outline-success col-md-9 offset-2 p-3 mb-2 bg-success text-white">Modificar</button>
+                                <button type="submit" class="btn btn-outline-success col-md-4 offset-2  mb-2 bg-success text-white">Modificar</button>
+                                <a  href=" {{ url('/') }}" class="btn bg-danger text-white col-md-3 offset-2" type="submit">Regresar</a>
 
                             </div>
 
